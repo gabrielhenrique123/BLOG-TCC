6f9e93cd56e09bbabc69e8933be061fd3253c01a
Gabrielhenrique123 <gabycristoffano@gmail.com>
2023-09-28 16:36:08 -0300

    Blog-tcc

---
+++ src/app/App.jsx
@@ -0,0 +1,17 @@
+import Home from "./components/Home/Home.jsx";
+import NavBar from "./components/Navbar/NavBar.jsx";
+import Login from "./pages/Login/Login.jsx";
+import Post from "./pages/Posts/Post.jsx";
+function App() {
+
+
+  return (
+    <div className='Screen'>
+    <Home/>
+    
+  </div>
+
+  )
+}
+
+export default App
